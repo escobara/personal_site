2e289654bc6633bb4e0749eff6540d1a77ede8e5
Rene <reneescobar07@gmail.com>
2014-09-20 14:49:49 -0400

    application layout with rspec bootstrap livereload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReneescobarNet::Application.config.secret_key_base = 'af2f678f68a802aa45bd07a36e8a96f114d9b5dc5006ad899e3f389b5a1394d78a13c84a4a66710265a387d493abfef7a73ac37c10c0e13b2ce6ee38910cafd2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReneescobarNet::Application.config.session_store :cookie_store, key: '_reneescobar_net_session'
